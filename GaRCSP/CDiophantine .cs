22ff4030e32d3e8d9789d542b434429fdce75a74
SayaUta <dmhanami@mail.ru>
2015-03-05 17:22:34 +0300

    1st - maybe it works
    olala

---
+++ GaRCSP/CDiophantine .cs
@@ -0,0 +1,401 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaRCSP
+{
+    struct PrecCons
+    {
+        public int act1;
+        public int act2;
+    };
+
+    struct gene
+    {
+        static int WORKCOUNT;
+        public int[] alleles;
+        public float likelihood;
+        public int genes(int tmpworkc)
+        {
+            /*for (int i = 0; i<100; i++) {
+                alleles[i] = -1;
+            }*/
+            WORKCOUNT = tmpworkc;
+            alleles = new int[tmpworkc];
+            likelihood = 0;
+            return 666;
+        }
+        //public: gene(int tmpworkc) : WORKCOUNT(tmpworkc){}
+        //int* alleles = new int[100];
+        //int fitness;
+        //int alleles[100];
+
+        // Test for equality.
+        public bool equal(gene gn)
+        {
+            for (int i = 0; i < WORKCOUNT; i++)
+            {
+                if (alleles[i] != gn.alleles[i]) return false;
+            }
+            return true;
+        }
+    };
+
+    class CDiophantine
+    {
+        int workcount;
+        int[] pDurat;
+        int[] pWorker;
+        /*int pDurat;
+         * int pWorker;*/
+        int workpreccount;
+        PrecCons[] pPrecCons;
+        int maxdur;
+        int newMaxDur;
+        gene[] population;//[MAXPOP];// Population.
+        public const int MAXPOP = 25;
+
+        public CDiophantine(int tworkcount, int[] tpDurat, int[] tpWorker, int tworkpreccount, PrecCons[] tpPrecCons, int tmaxdur)
+        {// Constructor 
+            workcount = tworkcount;
+            pDurat = tpDurat;
+            pWorker = tpWorker;
+            workpreccount = tworkpreccount;
+            pPrecCons = tpPrecCons;
+            maxdur = tmaxdur;
+            population = new gene[MAXPOP];
+            for (int i = 0; i < MAXPOP; i++)
+            {
+                population[i].genes(tworkcount);
+            }
+        }
+        public int Solve()
+        {// Solve the equation.
+            //int fitn = INT_MAX;
+
+            // Generate initial population.
+            Random rand = new Random();
+
+            for (int i = 0; i < MAXPOP; i++)
+            {// Fill the population with numbers between 
+                do
+                {
+                    //int tmp = rand() % (MAXDUR + 1);
+                    for (int j = 0; j < workcount; j++)
+                    {// 0 and the result.
+                        population[i].alleles[j] = /*rand() % (tmp + 1);//*/rand.Next(maxdur + 1);
+                    }
+                } while (ConstrCheck(population[i]) == 0);
+                //cout << "i = " << i << endl;
+            }
+
+            /*if (fitness = CreateFitnesses()) {
+            return fitness;
+            }*/
+
+            int iterations = 0;// Keep record of the iterations.
+            while (/*fitness != 0 || */iterations < 50)
+            {// Repeat until solution found, or over 50 iterations.
+                GenerateLikelihoods();// Create the likelihoods.
+                CreateNewPopulation();
+                /*if (fitness = CreateFitnesses()) {
+                return fitness;
+                }*/
+
+                iterations++;
+                //cout << "iterations = " << iterations << endl;
+                Console.WriteLine("iterations = " + iterations);
+            }
+
+            int ind = CreateFitnesses();
+            //return -1;
+            return ind;
+        }
+
+        // Returns a given gene.
+        public gene GetGene(int i)
+        {
+            return population[i];
+        }
+
+        int ConstrCheck(gene gn)
+        { //проверка ограничений
+            //int durat[WORKCOUNT] = { 1, 1, 2, 1, 1, 1, 0 };
+            //int worker[WORKCOUNT] = { 3, 1, 1, 2, 2, 2, 0 };
+            /*if (gn.alleles[0] + pDurat[0] > gn.alleles[6])
+                return 0;
+            if (gn.alleles[2] + pDurat[2] > gn.alleles[6])
+                return 0;
+            if (gn.alleles[5] + pDurat[5] > gn.alleles[6])
+                return 0;
+            if (gn.alleles[1] + pDurat[1] > gn.alleles[2])
+                return 0;
+            if (gn.alleles[3] + pDurat[3] > gn.alleles[4])
+                return 0;
+            if (gn.alleles[4] + pDurat[4] > gn.alleles[5])
+                return 0;*/
+            for (int i = 0; i < workpreccount; i++)
+            {
+                if (gn.alleles[pPrecCons[i].act1 - 1] + pDurat[pPrecCons[i].act1 - 1] > gn.alleles[pPrecCons[i].act2 - 1])
+                    return 0;
+            }
+            for (int i = 0; i < maxdur; i++)
+            {
+                int res = 0;
+                for (int j = 0; j < workcount; j++)
+                {
+                    if ((gn.alleles[j] <= i) && (gn.alleles[j] + pDurat[j] > i))
+                    {
+                        res = res + pWorker[j];
+                    }
+                }
+                if (res > 3)
+                    return 0;
+            }
+            return 1;
+        }
+        int Fitness(gene gn)
+        {// Fitness function.
+            return /*gn.fitness = */gn.alleles[workcount-1];
+        }
+        void GenerateLikelihoods()
+        {	// Generate likelihoods.
+            float multinv = MultInv();
+            float last = 0;
+            for (int i = 0; i < MAXPOP; i++)
+            {
+                population[i].likelihood = last = last + ((1 / ((float)Fitness(population[i])) / multinv) * 100);
+            }
+        }
+        float MultInv()
+        {// Creates the multiplicative inverse.
+            float sum = 0;
+            for (int i = 0; i < MAXPOP; i++)
+            {
+                sum += 1 / ((float)Fitness(population[i]));
+            }
+            return sum;
+        }
+        int CreateFitnesses()
+        {
+            int minfitness = 2147483647;
+            int fitness = 2147483647;
+            int minin = -1;
+            for (int i = 0; i < MAXPOP; i++)
+            {
+                fitness = Fitness(population[i]);
+                if (fitness < minfitness)
+                {
+                    minfitness = fitness;
+                    minin = i;
+                }
+            }
+            return minin;
+        }
+        void CreateNewPopulation()
+        {
+            //cout << "7th point" << endl;
+            gene[] temppop = new gene[MAXPOP];
+            /*gene* temppop = (gene*)operator new(sizeof(gene)* MAXPOP);
+            for (int i = 0; i < MAXPOP; ++i)
+                new(&temppop[i]) gene(workcount);*/
+            //vector<gene> temppop(MAXPOP);
+            //map <int,int> mapfit;
+            PrecCons[] mapfit = new PrecCons[MAXPOP];
+            for (int i = 0; i < MAXPOP; i++)
+            {
+                mapfit[i].act1 = i;
+                mapfit[i].act2 = Fitness(population[i]);
+            }
+            Array.Sort(mapfit, new Comparison<PrecCons>((a, b) => a.act2.CompareTo(b.act2)));
+
+
+            //cout << "8th point" << endl;
+            for (int i = 0; i < 12; i++)
+            {
+                //map <int, int>::iterator cur;
+                //cout << "start" << endl;
+                //for (cur = mapfit.begin(); cur != mapfit.end(); cur++){
+                //cout << "map " << Fitness(population[(*cur).second]) << endl;
+                temppop[i] = population[mapfit[i].act1];
+            }
+            int[] tmpnwp = new int[MAXPOP];
+            for (int i = 0; i < MAXPOP; i++)
+            {
+                tmpnwp[i] = 0;
+            }
+
+            for (int i = 0; i < 12; i++)
+            {
+                int minfitness = 2147483647;
+                int fitness = 2147483647;
+                int minin = -1;
+                for (int j = 0; j < MAXPOP; j++)
+                {
+                    fitness = Fitness(population[j]);
+                    if ((fitness < minfitness) && (tmpnwp[j] == 0))
+                    {
+                        minfitness = fitness;
+                        minin = j;
+                    }
+                }
+                tmpnwp[minin] = 1;
+                temppop[i] = population[minin];
+            }
+
+            newMaxDur = Fitness(temppop[0]);
+            Random rand = new Random();
+            //cout << "9th point" << endl;
+            for (int i = 12; i < 22; i++)
+            {
+                int parent1 = 0, parent2 = 0, iterations = 0;
+                while (parent1 == parent2 || population[parent1].equal(population[parent2]))
+                {
+                    //cout << "20th point" << endl;
+                    parent1 = GetIndex((float)(rand.Next(101)));
+                    parent2 = GetIndex((float)(rand.Next(101)));
+                    if (++iterations > 25) break;
+                }
+
+                temppop[i] = Breed(parent1, parent2);// Create a child.
+                //cout << "i = " << i << endl;
+            }
+
+            for (int i = 22; i < MAXPOP; i++)
+            {
+                //int oops = 0;
+                do
+                {
+                    //Console.WriteLine("booo");
+                    //cout << "booo" << endl;
+                    //if (oops < 50) {
+                    temppop[i].genes(workcount);
+                    for (int j = 0; j < workcount; j++)
+                    {
+                        temppop[i].alleles[j] = rand.Next(newMaxDur + 2);
+                    }
+                    //}
+                    /*else {
+                        for (int j = 0; j < workcount; j++) {
+                            temppop[i].alleles[j] = rand() % (newMaxDur + 2);
+                        }
+                    }*/
+                } while (ConstrCheck(temppop[i]) == 0);
+            }
+
+            for (int i = 0; i < MAXPOP; i++) population[i] = temppop[i];
+        }
+        int GetIndex(float val)
+        {
+            float last = 0;
+            for (int i = 0; i < MAXPOP; i++)
+            {
+                if (last <= val && val <= population[i].likelihood) return i;
+                else last = population[i].likelihood;
+            }
+
+            return 4;
+        }
+
+        gene Breed(int p1, int p2)
+        {
+            //cout << "3rd point" << endl;
+            Random rand = new Random();
+            int crossover = rand.Next(workcount);// Create the crossover point (not first).
+
+            //int first = rand() % 100;// Which parent comes first?
+
+            gene child = population[p1];// Child is all first parent initially.
+
+            int[] tmpbr = new int[workcount];// = { 0, 0, 0, 0, 0, 0, 0 };
+            for (int i = 0; i < workcount; i++)
+            {
+                tmpbr[i] = 0;
+            }
+
+            int flag = 1;
+
+            for (int i = 0; i < workcount; i++)
+            {
+                if (child.alleles[i] <= child.alleles[crossover])
+                    tmpbr[i] = 1;
+                else
+                    flag = 0;
+            }
+            //cout << "6th point" << endl;
+            while (flag == 0)
+            {
+                int minal = 2147483647;
+                int minin = -1;
+                for (int i = 0; i < workcount; i++)
+                {
+                    if ((tmpbr[i] == 0) && (population[p2].alleles[i] < minal))
+                    {
+                        minal = population[p2].alleles[i];
+                        minin = i;
+                    }
+                }
+                child.alleles[minin] = population[p2].alleles[minin];
+                if (ConstrCheck(child) == 0)
+                {
+                    int oops = 0;
+                    do
+                    {
+                        if (oops < 50)
+                        {
+                            child.alleles[minin] = rand.Next(maxdur + 1);
+                            oops = oops + 1;
+                            //cout << "10th point" << endl;
+                        }
+                        else
+                        {
+                            do
+                            {
+                                for (int j = 0; j < workcount; j++)
+                                {
+                                    child.alleles[j] = rand.Next(maxdur + 1);
+                                }
+                            } while (ConstrCheck(child) == 0);
+                        }
+                    } while (ConstrCheck(child) == 0);
+                    //cout << "1st point" << endl;
+                }
+                tmpbr[minin] = 1;
+                flag = 1;
+                for (int i = 0; i < workcount; i++)
+                {
+                    if (tmpbr[i] == 0)
+                        flag = 0;
+                }
+                //cout << "flag = " << flag << endl;
+            }
+            //cout << "5th point" << endl;
+            int mutation = rand.Next(workcount);// Create the crossover point (not first).
+            if (rand.Next(101) < 15)
+            {
+                child.alleles[mutation] = rand.Next(maxdur + 1);
+                if (ConstrCheck(child) == 0)
+                {
+                    do
+                    {
+                        child.alleles[mutation] = rand.Next(maxdur + 1);
+                        //cout << "2nd point" << endl;
+                    } while (ConstrCheck(child) == 0);
+                }
+            }
+
+            /*int initial = 0, final = 3;// The crossover boundaries.
+            if (first < 50) initial = crossover;	// If first parent first. start from crossover.
+            else final = crossover + 1;// Else end at crossover.
+
+            for (int i = initial; i<final; i++) {// Crossover!
+            child.alleles[i] = population[p2].alleles[i];
+            if (rand() % 101 < 5) child.alleles[i] = rand() % (result + 1);
+            }*/
+            //cout << "4th point" << endl;
+            return child;// Return the kid...
+        }
+    }
+}
---
+++ GaRCSP/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GaRCSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int workcount;
+            /*cout << "Insert number of activities: ";
+             * cin >> workcount;*/
+            workcount = 8;
+            int[] pDurat = new int[workcount];
+            /*for (int i = 0; i < workcount; i++){
+             * cout << "Insert " << i+1 << " activity duration: ";
+             * cin >> pDurat[i];
+             * }*/
+            pDurat[0] = 2;
+            pDurat[1] = 6;
+            pDurat[2] = 4;
+            pDurat[3] = 2;
+            pDurat[4] = 2;
+            pDurat[5] = 4;
+            pDurat[6] = 2;
+            pDurat[7] = 0;
+            int[] pWorker = new int[workcount];
+            /*for (int i = 0; i < workcount; i++){
+             * cout << "Insert " << i + 1 << " activity resource usage: ";
+             * cin >> pWorker[i];
+             * }*/
+            pWorker[0] = 1;
+            pWorker[1] = 1;
+            pWorker[2] = 1;
+            pWorker[3] = 1;
+            pWorker[4] = 1;
+            pWorker[5] = 1;
+            pWorker[6] = 1;
+            pWorker[7] = 0;
+            int workpreccount;
+            workpreccount = 10;
+            /*cout << "Insert number of precedence constraints: ";
+             * cin >> workpreccount;*/
+            PrecCons[] pPrecCons = new PrecCons[workpreccount];
+            /*for (int i = 0; i < workpreccount; i++){
+             * cout << "Insert " << i + 1 << " precedence constraints: ";
+             * cin >> pPrecCons[i].act1 >> pPrecCons[i].act2;
+             * }*/
+            pPrecCons[0].act1 = 1;
+            pPrecCons[1].act1 = 1;
+            pPrecCons[2].act1 = 1;
+            pPrecCons[3].act1 = 4;
+            pPrecCons[4].act1 = 2;
+            pPrecCons[5].act1 = 3;
+            pPrecCons[6].act1 = 3;
+            pPrecCons[7].act1 = 5;
+            pPrecCons[8].act1 = 6;
+            pPrecCons[9].act1 = 7;
+            pPrecCons[0].act2 = 4;
+            pPrecCons[1].act2 = 2;
+            pPrecCons[2].act2 = 3;
+            pPrecCons[3].act2 = 7;
+            pPrecCons[4].act2 = 7;
+            pPrecCons[5].act2 = 5;
+            pPrecCons[6].act2 = 6;
+            pPrecCons[7].act2 = 7;
+            pPrecCons[8].act2 = 7;
+            pPrecCons[9].act2 = 8;
+            int maxdur;
+            /*cout << "Insert expected min duration: ";
+             * cin >> maxdur;*/
+            maxdur = 50;
+            CDiophantine dp = new CDiophantine(workcount, pDurat, pWorker, workpreccount, pPrecCons, maxdur);
+
+            int ans;
+            ans = dp.Solve();
+            /*if (ans == -1) {
+             * cout << "No solution found." << endl;
+             * }
+             * else {
+             * gene gn = dp.GetGene(ans);
+             * cout << "The solution set to a+2b+3c+4d=30 is:\n";
+             * cout << "a = " << gn.alleles[0] << "." << endl;
+             * cout << "b = " << gn.alleles[1] << "." << endl;
+             * cout << "c = " << gn.alleles[2] << "." << endl;
+             * cout << "d = " << gn.alleles[3] << "." << endl;
+             * }*/
+            gene gn = dp.GetGene(ans);
+            Console.WriteLine("1 = " + gn.alleles[0] + "\r\n");
+            Console.WriteLine("2 = " + gn.alleles[1] + "\r\n");
+            Console.WriteLine("3 = " + gn.alleles[2] + "\r\n");
+            Console.WriteLine("4 = " + gn.alleles[3] + "\r\n");
+            Console.WriteLine("5 = " + gn.alleles[4] + "\r\n");
+            Console.WriteLine("6 = " + gn.alleles[5] + "\r\n");
+            Console.WriteLine("7 = " + gn.alleles[6] + "\r\n");
+            Console.WriteLine("8 = " + gn.alleles[7] + "\r\n");
+            /*cout << "a = " << gn.alleles[0] << "." << endl;
+            cout << "b = " << gn.alleles[1] << "." << endl;
+            cout << "c = " << gn.alleles[2] << "." << endl;
+            cout << "d = " << gn.alleles[3] << "." << endl;
+            cout << "e = " << gn.alleles[4] << "." << endl;
+            cout << "f = " << gn.alleles[5] << "." << endl;
+            cout << "g = " << gn.alleles[6] << "." << endl;
+            system("pause");*/
+            Console.ReadKey();
+        }
+    }
+}
